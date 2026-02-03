2362191c3eb89b8bac70740668ffe7e6831af53e
cmrohityadav <cmrohityadav23@gmail.com>
2026-02-03 21:43:46 +0530

    signal and slot

---
+++ 04_signal_slot/mainwindow.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->horizontalSlider_2,SIGNAL(valueChanged(int)),ui->progressBar_2,SLOT(setValue(int)));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->label->setText("Hello rohit ji");
+}
+
---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Qt
+
+# Layout
+
+## Vertical
+- Arranges widgets from top to bottom
+## Horizontal
+- Arranges widgets from left to right
+## Grid
+- Arranges widgets in rows and columns
+
+# Signal
+- Ek Request or notification
+- Just Like EVENT - click,pressed, etc
+# Slot
+- Ek Response
+- EVENT pe kya kare to hum ek function likhte hai
+- We write a function to define what should happen when an event occurs
